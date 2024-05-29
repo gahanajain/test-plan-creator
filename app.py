ca2860e52bedd6d787ab338ed013a8f7f8012388
gahanajain <70436638+gahanajain@users.noreply.github.com>
2024-05-29 17:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from bedrock_integration import invoke_claude, build_claude_prompt
 from aws_session import assume_role
 from sheets_manager import initialize_sheets_service, duplicate_template_sheet, update_sheet_with_data
 from utils import verify_slack_signature
-from parse_text import parse_feature_text, is_greeting, parse_claude_response
+from parse_text import is_greeting, parse_claude_response, remove_curly_brace_pairs
 from encryption import decrypt_file
 from datetime import date
 from threading import Lock
 import html
 import json
+import traceback
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -113,109 +114,145 @@ def slack_interactions():
 
 def handle_slack_event(event_data):
     global conversation_states
-    
-    user_id = event_data['user']
-    channel_id = event_data['channel']
-    text = event_data.get('text', '').strip()
-    event_ts = event_data.get('event_ts', '')
 
-    with state_lock:
+    try:     
+        user_id = event_data['user']
+        channel_id = event_data['channel']
+        text = event_data.get('text', '').strip()
+        event_ts = event_data.get('event_ts', '')
+
+        with state_lock:
         # Fetch the user state, or create a new state if it doesn't exist
-        user_state = conversation_states.setdefault(
-            user_id, {
-                'status': 'new',
-                'feature_details': None,
-                'selected_tabs': None,
-                'last_event_ts': '0',
-                'last_bot_message': None
-                })
+            user_state = conversation_states.setdefault(
+                user_id, {
+                    'status': 'new',
+                    'feature_name': None,
+                    'feature_details': None,
+                    'feature_criteria': None,
+                    'selected_tabs': None,
+                    'last_event_ts': '0',
+                    'last_bot_message': None
+                    })
 
-        # If the received event is older than the last event processed for the user, discard it
-        if event_ts <= user_state['last_event_ts']:
-            return
-        
-        # Update the last event timestamp to the current event's timestamp
-        user_state['last_event_ts'] = event_ts
-        
-        if escape_html(text) == user_state['last_bot_message']:
-            # Prevent sending the same message repeatedly
-            return
-
-        if is_greeting(text) and user_state['status'] in ['new', 'greeted']:
-            send_greeting(channel_id, user_id)
-            user_state['status'] = 'greeted'
+            # If the received event is older than the last event processed for the user, discard it
+            if event_ts <= user_state['last_event_ts']:
+                return
             
-        elif user_state['status'] == 'greeted' or (user_state['status'] == 'awaiting_feature_info' and not user_state['feature_details']):
-                feature_name, feature_details, acceptance_criteria = parse_feature_text(text)
-                user_state['feature_details'] = (feature_name, feature_details, acceptance_criteria)
-                user_state['status'] = 'awaiting_tab_selection'
-                ask_for_tabs_update(channel_id, user_id)
-                
-        # Check if tabs are selected and feature details have been provided
-        if user_state['status'] == 'tabs_selected' and user_state['feature_details'] is not None:
-            feature_name, feature_details, acceptance_criteria = user_state['feature_details']
-            selected_tabs = user_state['selected_tabs']
-            process_feature_details(channel_id, user_id, selected_tabs, feature_name, 
-                                    feature_details, acceptance_criteria)
+            # Update the last event timestamp to the current event's timestamp
+            user_state['last_event_ts'] = event_ts
+            if escape_html(text) == user_state['last_bot_message']:
+                return
 
-        conversation_states[user_id] = user_state  # Save state changes
+            if is_greeting(text):
+                send_greeting(channel_id, user_id)
+                user_state['status'] = 'awaiting_feature_name'
+
+            elif user_state['status'] == 'awaiting_feature_name':
+                user_state['feature_name'] = remove_curly_brace_pairs(text)
+                user_state['status'] = 'awaiting_feature_details'
+                ask_for_feature_details(channel_id, user_id)
+
+            elif user_state['status'] == 'awaiting_feature_details':
+                user_state['feature_details'] = remove_curly_brace_pairs(text)
+                ask_for_extra_details(channel_id, user_id)   
+                user_state['status'] = 'awaiting_feature_criteria'
+                
+            elif user_state['status'] == 'awaiting_feature_criteria':
+                user_state['feature_criteria'] = remove_curly_brace_pairs(text)
+                ask_for_tabs_update(channel_id, user_id)
+                user_state['status'] = 'tabs_selected'
+
+            elif user_state['status'] == 'tabs_selected':
+                selected_tabs = user_state['selected_tabs']
+                feature_name = user_state['feature_name']
+                feature_details = user_state['feature_details']
+                feature_criteria = user_state['feature_criteria']
+                process_feature_details(channel_id, user_id, selected_tabs, feature_name, 
+                                        feature_details, feature_criteria)
+            
+            user_state['last_event_ts'] = event_ts
+            conversation_states[user_id] = user_state
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def process_feature_details(channel_id, user_id, selected_tabs, feature_name, 
-                                    feature_details, acceptance_criteria):
-    global conversation_states
+                                    feature_details, feature_criteria):
+    try: 
+        global conversation_states
+        # Assume the role just before the AWS service call
+        role_arn = "arn:aws:iam::511738828901:role/test-plan-creator"  
+        external_id = "test-plan-creator" 
 
-    # Assume the role just before the AWS service call
-    role_arn = "arn:aws:iam::511738828901:role/test-plan-creator"  
-    external_id = "test-plan-creator" 
+        session_credentials = assume_role(role_arn, external_id)
+        if session_credentials:
+            aws_access_key_id_temp = session_credentials['AccessKeyId']
+            aws_secret_access_key_temp = session_credentials['SecretAccessKey']
+            aws_session_token_temp = session_credentials['SessionToken']
 
-    session_credentials = assume_role(role_arn, external_id)
-    if session_credentials:
-        aws_access_key_id_temp = session_credentials['AccessKeyId']
-        aws_secret_access_key_temp = session_credentials['SecretAccessKey']
-        aws_session_token_temp = session_credentials['SessionToken']
-
-        new_sheet_id = duplicate_template_sheet(config['google_service_account_info'], TEMPLATE_SHEET_ID, feature_name)
-        print("new_sheet_id:", new_sheet_id)
-        
-        for tab in selected_tabs:
-            tab_name = tab_mapping[tab] 
-            send_slack_message(slack_client, channel_id, f"Getting test cases for {tab_name} tab")
-            prompt = build_claude_prompt(feature_name, feature_details, acceptance_criteria, tab_name)
-            raw_response = invoke_claude(prompt, aws_access_key_id_temp, aws_secret_access_key_temp, aws_session_token_temp)
-            print("raw_response:", raw_response)
-            send_slack_message(slack_client, channel_id, f"Successfully built test cases for {tab_name} tab")
-            parsed_data = parse_claude_response(raw_response)  # This function should return a 2D array
-            # Find the tab in the spreadsheet and update it with parsed data
-            last_col_num = len(parsed_data[0])
-            end_column_letter = chr(ord('A') + last_col_num - 1)
-            formatted_range = f"'{tab_name}'!A3:{end_column_letter}{len(parsed_data) + 2}"  # +2 to account for header rows
-            print("formatted_range:", formatted_range)
-            update_sheet_with_data(sheets_service, new_sheet_id, formatted_range, parsed_data)  
+            new_sheet_id = duplicate_template_sheet(config['google_service_account_info'], TEMPLATE_SHEET_ID, feature_name)
+            print("New Sheet ID:", new_sheet_id)
+            
+            for tab in selected_tabs:
+                tab_name = tab_mapping[tab] 
+                send_slack_message(slack_client, channel_id, f"Getting test cases for {tab_name} tab")
+                prompt = build_claude_prompt(feature_name, feature_details, feature_criteria, tab_name)
+                raw_response = invoke_claude(prompt, aws_access_key_id_temp, aws_secret_access_key_temp, aws_session_token_temp)
+                send_slack_message(slack_client, channel_id, f"Successfully built test cases for {tab_name} tab")
+                parsed_data = parse_claude_response(raw_response)  # This function should return a 2D array
+                # Find the tab in the spreadsheet and update it with parsed data
+                last_col_num = len(parsed_data[0])
+                end_column_letter = chr(ord('A') + last_col_num - 1)
+                formatted_range = f"'{tab_name}'!A3:{end_column_letter}{len(parsed_data) + 2}"  # +2 to account for header rows
+                update_sheet_with_data(sheets_service, new_sheet_id, formatted_range, parsed_data)  
 
 
-        sheet_url = f"https://docs.google.com/spreadsheets/d/{new_sheet_id}"
-        sheet_message = f"Here's the Google Sheet with test cases: {sheet_url}"
-        send_slack_message(slack_client, channel_id, sheet_message)    
+            sheet_url = f"https://docs.google.com/spreadsheets/d/{new_sheet_id}"
+            sheet_message = f"Here's the Google Sheet with test cases: {sheet_url}"
+            send_slack_message(slack_client, channel_id, sheet_message)    
 
-        user_state = conversation_states.get(user_id, {})
-        user_state['last_bot_message'] = sheet_message
-        user_state['status'] = 'new'
-        conversation_states[user_id] = user_state   
+            user_state = conversation_states.get(user_id, {})
+            user_state['last_bot_message'] = sheet_message
+            user_state['status'] = 'new'
+            conversation_states[user_id] = user_state   
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
 
 def send_greeting(channel_id, user_id):
     global conversation_states
-
+    
     welcome_message = (
         ":wave: Hi! Welcome to Test Plan Creator.\n"
-        "Please provide the following details in this format:\n\n"
-        "Feature name: <name>\n"
-        "Feature details: <details>\n"
-        "Acceptance criteria: <criteria>"
+        "To create a test plan, I’ll start by asking a few questions about the feature. "
+        "What is the name of the feature you're working on?"
     )
     send_slack_message(slack_client, channel_id, welcome_message)
-
     user_state = conversation_states.get(user_id, {})
     user_state['last_bot_message'] = welcome_message
+    conversation_states[user_id] = user_state
+    
+def ask_for_feature_details(channel_id, user_id):
+    global conversation_states
+    
+    # Since we've already asked for the feature name in `send_greeting`, move on to the next question.
+    message = "Please provide the details for the feature."
+    send_slack_message(slack_client, channel_id, message)
+    
+    user_state = conversation_states.get(user_id, {})
+    user_state['last_bot_message'] = message
+    conversation_states[user_id] = user_state
+
+def ask_for_extra_details(channel_id, user_id):
+    global conversation_states
+
+    message = "Could you please provide any additional details/acceptance criteria/API Information (if any) for the feature? Else just reply with N/A"
+    send_slack_message(slack_client, channel_id, message)
+    # Update the user state to expect acceptance criteria.
+    
+    user_state = conversation_states.get(user_id, {})
+    user_state['last_bot_message'] = message
     conversation_states[user_id] = user_state
 
 def escape_html(text):
@@ -229,7 +266,7 @@ def ask_for_tabs_update(channel_id, user_id):
         },
         {
             "type": "section",
-            "text": {"type": "mrkdwn", "text": " "},  # This `text` is required, but the space will not show.
+            "text": {"type": "mrkdwn", "text": " "},
             "accessory": {
                 "type": "checkboxes",
                 "options": [
@@ -268,7 +305,6 @@ def ask_for_tabs_update(channel_id, user_id):
             )
     except SlackApiError as e:
         print(f"Error sending interactive message: {e.response['error']}")
-
 
 if __name__ == '__main__':
     app.run(debug=True)  
